41124253fc8b1767ba4ffae9882bd90cbc9edfd6
ComfortPrince <comfort.prince.hluyo@gmail.com>
2024-03-23 21:00:05 +0200

    Fixed the Cancel button. Made it behave in the same way as the browser back button. Used the navigate hook.

--- src/routes/edit.jsx
+++ src/routes/edit.jsx
@@ -1,7 +1,8 @@
 import { 
   Form, 
   useLoaderData,
-  redirect
+  redirect,
+  useNavigate
 } from "react-router-dom";
 
 import { updateContact } from '../contacts.js'
@@ -15,6 +16,7 @@ export async function action({ request, params }) {
 
 export default function EditContact() {
   const { contact } = useLoaderData();
+  const navigate = useNavigate()
 
   return (
     <Form method="post" id="contact-form">
@@ -64,7 +66,12 @@ export default function EditContact() {
       </label>
       <p>
         <button type="submit">Save</button>
-        <button type="button">Cancel</button>
+        <button 
+          type="button"
+          onClick={() => {
+            navigate(-1);
+          }}
+        >Cancel</button>
       </p>
     </Form>
   );
