241516b3d2c526586e3a13a8dd3336a3161af5ff
ComfortPrince <comfort.prince.hluyo@gmail.com>
2024-03-23 14:24:12 +0200

    Added route loaders to the contact route. Implemented contact creation using root routes' action prop

--- src/index.js
+++ src/index.js
@@ -7,8 +7,15 @@ import {
 
 import './index.css';
 
-import Root, { loader as rootLoader } from "./routes/root";
-import Contact from "./routes/contact"
+import Root, { 
+	loader as rootLoader, 
+	action as rootAction 
+} from "./routes/root";
+
+import Contact, {
+	loader as contactLoader
+} from "./routes/contact"
+
 import ErrorPage from "./error-page";
 
 const router = createBrowserRouter([
@@ -17,10 +24,12 @@ const router = createBrowserRouter([
 		element: <Root />,
 		errorElement: <ErrorPage />,
 		loader: rootLoader,
+		action: rootAction,
 		children: [
 			{
 				path: '/contacts/:contactId',
 				element: <Contact />,
+				loader: contactLoader,
 			},
 		]
 	}
--- src/routes/root.jsx
+++ src/routes/root.jsx
@@ -2,9 +2,15 @@ import {
   Outlet, 
   Link,
   useLoaderData,
+  Form,
 } from 'react-router-dom'
 
-import { getContacts } from "../contacts";
+import { getContacts, createContact } from "../contacts";
+
+export async function action(){
+  const contact = await createContact()
+  return { contact }
+}
 
 export default function Root() {
   const { contacts } = useLoaderData();
@@ -31,9 +37,9 @@ export default function Root() {
               aria-live="polite"
             ></div>
           </form>
-          <form method="post">
+          <Form method="post">
             <button type="submit">New</button>
-          </form>
+          </Form>
         </div>
         <nav>
           {contacts.length ? (
